5620394135cae0b3e399296ffc0fc11c7186a30d
Xiaomei Mi <135629372+xm2325@users.noreply.github.com>
2026-08-28 19:13:38 +0100

    feat: export production QC and reviewer-bundle APIs

--- src/rwe_programming/__init__.py
+++ src/rwe_programming/__init__.py
@@ -15,6 +15,12 @@ from .validation import (
 )
 from .sensitivity import bootstrap_hr, proportional_hazards_diagnostic
 from .report import build_report
+from .study_config import DEFAULT_CONFIG, StudyConfig
+from .qc import cohort_attrition, qc_manifest, run_qc_registry, write_qc_manifest
+from .metadata import data_dictionary_frame
+from .independent_cox import fit_independent_weighted_cox, reconcile_cox
+from .deliverables import table1_balance, table2_outcomes, table3_primary_effect, write_deliverables
+from .bundle import build_reviewer_bundle
 
 __all__ = [
     "make_synthetic_cohort",
@@ -31,4 +37,18 @@ __all__ = [
     "bootstrap_hr",
     "proportional_hazards_diagnostic",
     "build_report",
+    "StudyConfig",
+    "DEFAULT_CONFIG",
+    "cohort_attrition",
+    "run_qc_registry",
+    "qc_manifest",
+    "write_qc_manifest",
+    "data_dictionary_frame",
+    "fit_independent_weighted_cox",
+    "reconcile_cox",
+    "table1_balance",
+    "table2_outcomes",
+    "table3_primary_effect",
+    "write_deliverables",
+    "build_reviewer_bundle",
 ]
